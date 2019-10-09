e44193eb07d965fe8a268c914208fd4381299292
DesktopGit <markusriisebbesen@gmail.com>
2019-10-09 21:42:41 +0200

    gg

--- apply.php
+++ apply.php
@@ -1,220 +1,25 @@
-<!DOCTYPE HTML>
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
 
-<meta name=”viewport” content=”width=device-width, initial-scale=1″>
-<link rel="icon" href="https://imgur.com/0f3ry71.png" type="image/x-icon"/>
 
-<html>
-	<head>
-		<title>Vilysia</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="assets/css/main.css" />
-	</head>
-	<body class="homepage is-preload">
-		<div id="page-wrapper">
+$email_from = 'mc.vilysia@gmail.com';
 
-			<!-- Header -->
-				<section id="header" class="wrapper">
+$email_subject = "Staff Application";
 
-					<!-- Logo -->
-						<div id="logo">
-		                <h1>Apply</h1>
-<!--							<p>Vilysia</p> -->
-						</div>
+$email_body = "User Name: $name.\n".
+"User Email: $visitor_email.\n".
+"User Message: $message.\n";
 
-					<!-- Nav -->
-						<nav id="nav">
-							<ul>
-								<li class="current, primarycolor"><a href="index.html">Home</a></li>
-								<li><a href="vote.html">Vote</a></li>
-								<li><a href="http://buy.vilysia.com/category/1415005" target="_blank">Ranks</a></li>
-								<li>
-									<a class="primarycolor" style="color:#31BDDD">More</a>
-									<ul>
-										<li><a class="primarycolor" href="#" style="color:#31BDDD">Apply</a></li>
-<!--										<li><a href="#">Magna veroeros</a></li>
-										<li><a href="#">Etiam nisl</a></li>
-										<li>
-											<a href="#">Sed consequat</a>
-											<ul>
-												<li><a href="#">Lorem dolor</a></li>
-												<li><a href="#">Amet consequat</a></li>
-												<li><a href="#">Magna phasellus</a></li>
-												<li><a href="#">Etiam nisl</a></li>
-												<li><a href="#">Sed feugiat</a></li> -->
-											</ul>
-								</ul>
-								</li>
-						</nav>
 
-				</section>
+$to = "markusriisebbesen@gmail.com";
 
-			<!-- Intro -->
-				<section id="intro" class="wrapper style1">
-					<div class="title">mc.vilysia.com</div>
-					<div class="container">
-						<p class="style2">
-			So you want a staff role?<br><br>
-			<span style="font-size:50%; font-weight:normal; letter-spacing:0.03em; color:#C7C7C7;">
-			No problem! We'd love to take you into consideration,<br>
-			but remember, you've joined a difficult contest 😜<br> </span>
-					</p>
-						</div>
-						<div class="" style="margin-left:25%;margin-right:65%;">
-							<div class="col-12-medium">
+$headers = "From: $email_from \r\n";
 
-								<!-- Contact Form -->
-									<section>
-										<form id="applyform" method="post" action="application.php">
-										 <h2 style="color:#e97770; text-align:left; width:300px; margin-bottom:5%; font-size:125%;">Ingame Name</h2>
-											<div class="row gtr-50">
-												<div class="col-12">
-													<input class="formwidth" type="text" name="name" id="name" placeholder="Notch" required/>
-												</div>
-												<div class="col-12">
-												<h2 style="color:#e97770; text-align:left; width:300px; margin-bottom:5%; font-size:125%;">Email</h2>
-													<input class="formwidth" type="text" name="email" id="email" placeholder="Yourmail@gmail.com" required/>
-												</div>
-												<div class="col-12">
-												<h2 style="color:#e97770; text-align:left; width:300px; margin-bottom:5%; font-size:125%;">Message</h2>
-													<textarea class="formwidth" name="message" id="message" placeholder="Message..." rows="4" required></textarea>
-												</div>
-												<div class="col-12">
-													<ul class="actions">
-														<li><input type="submit" class="style1" value="Send" /></li>
-													</ul>
-												</div>
-											</div>
-										</form>
-									</section>
-							</div>
-					</div>
-				</section>
+$headers .= "Reply To: $visitor_email \r\n";
 
-<!-- ####################################################################
-			<!-- Main ----Erstat med pil----
-				<section id="main" class="wrapper style2">
-					<div class="title">The Details</div>
-					<div class="container">
+mail($to,email_subject,$email_body,$headers);
 
-						<!-- Image ----Erstat med pil----
-							<a href="#" class="image featured">
-								<img src="images/pic01.jpg" alt="" />
-							</a>
-
-						<!-- Features ----Erstat med pil----
-							<section id="features">
-								<header class="style1">
-									<h2>Dolor consequat feugiat amet veroeros</h2>
-									<p>Feugiat dolor nullam orci pretium phasellus justo</p>
-								</header>
-								<div class="feature-list">
-									<div class="row">
-										<div class="col-6 col-12-medium">
-											<section>
-												<h3 class="icon fa-comment">Mattis velit diam vulputate</h3>
-												<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate et magna feugiat laoreet vel velit lorem.</p>
-											</section>
-										</div>
-										<div class="col-6 col-12-medium">
-											<section>
-												<h3 class="icon solid fa-sync">Lorem ipsum dolor sit veroeros</h3>
-												<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate et magna feugiat laoreet vel velit lorem.</p>
-											</section>
-										</div>
-										<div class="col-6 col-12-medium">
-											<section>
-												<h3 class="icon fa-image">Pretium phasellus justo lorem</h3>
-												<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate et magna feugiat laoreet vel velit lorem.</p>
-											</section>
-										</div>
-										<div class="col-6 col-12-medium">
-											<section>
-												<h3 class="icon solid fa-cog">Tempus sed pretium orci</h3>
-												<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate et magna feugiat laoreet vel velit lorem.</p>
-											</section>
-										</div>
-										<div class="col-6 col-12-medium">
-											<section>
-												<h3 class="icon solid fa-wrench">Aliquam consequat et feugiat</h3>
-												<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate et magna feugiat laoreet vel velit lorem.</p>
-											</section>
-										</div>
-										<div class="col-6 col-12-medium">
-											<section>
-												<h3 class="icon solid fa-check">Dolore laoreet aliquam mattis</h3>
-												<p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate et magna feugiat laoreet vel velit lorem.</p>
-											</section>
-										</div>
-									</div>
-								</div>
-								<ul class="actions special">
-									<li><a href="#" class="button style1 large">Get Started</a></li>
-									<li><a href="#" class="button style2 large">More Info</a></li>
-								</ul>
-							</section>
-
-					</div>
-				</section>
-
-			<!-- Highlights ----Erstat med pil----
-				<section id="highlights" class="wrapper style3">
-					<div class="title">The Endorsements</div>
-					<div class="container">
-						<div class="row aln-center">
-							<div class="col-4 col-12-medium">
-								<section class="highlight">
-									<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
-									<h3><a href="#">Aliquam diam consequat</a></h3>
-									<p>Eget mattis at, laoreet vel amet sed velit aliquam diam ante, dolor aliquet sit amet vulputate mattis amet laoreet lorem.</p>
-									<ul class="actions">
-										<li><a href="#" class="button style1">Learn More</a></li>
-									</ul>
-								</section>
-							</div>
-							<div class="col-4 col-12-medium">
-								<section class="highlight">
-									<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
-									<h3><a href="#">Nisl adipiscing sed lorem</a></h3>
-									<p>Eget mattis at, laoreet vel amet sed velit aliquam diam ante, dolor aliquet sit amet vulputate mattis amet laoreet lorem.</p>
-									<ul class="actions">
-										<li><a href="#" class="button style1">Learn More</a></li>
-									</ul>
-								</section>
-							</div>
-							<div class="col-4 col-12-medium">
-								<section class="highlight">
-									<a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
-									<h3><a href="#">Mattis tempus lorem</a></h3>
-									<p>Eget mattis at, laoreet vel amet sed velit aliquam diam ante, dolor aliquet sit amet vulputate mattis amet laoreet lorem.</p>
-									<ul class="actions">
-										<li><a href="#" class="button style1">Learn More</a></li>
-									</ul>
-								</section>
-							</div>
-						</div>
-					</div>
-				</section>
- #################################################################### -->
-			<!-- Footer -->
-				<section id="footer" class="">
-									<section class="feature-list">
-										<!--<div class="row">   CENTRER--> 
-											<div class="col-5">
-											</div>
-												<section>
-													<h3>Social/Contact</h3>
-														<h4>mc.vilysia@gmail.com</h4>
-														<h4>discord.gg/TagrbJ2</h4>
-														<p>Thanks to html5up.net!</a>
-												</section>
-		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.dropotron.min.js"></script>
-			<script src="assets/js/browser.min.js"></script>
-			<script src="assets/js/breakpoints.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
-
-	</body>
-</html>
+header("Location: index.html");
+?>
